5a69e04bad072f3aee497cec13754c897ed1e614
katie.weber@powerhrg.com <katie.weber@powerhrg.com>
2018-05-02 16:55:59 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -103,5 +103,13 @@ def winner(board)
   end
 end
 
-def play
+def play(board)
+  until over?(board)
+    turn(board)
+  end
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cat's Game!"
+  end
 end
